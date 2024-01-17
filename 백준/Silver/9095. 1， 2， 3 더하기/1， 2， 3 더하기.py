263f3e18bfc75f9831faa216b862c1c217ebda3c
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-01-17 16:22:40 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,12 +1,8 @@
-import sys
+n = int(input())
 
-input = sys.stdin.readline
-
-t = int(input())
-
-for _ in range(t):
-    dp = [0, 1, 2, 4]
-    n = int(input())
-    for i in range(4, n + 1):
+dp = [0, 1, 2, 4]
+for _ in range(n):
+    t = int(input()) 
+    for i in range(len(dp), t + 1): 
         dp.append(dp[i - 1] + dp[i - 2] + dp[i - 3])
-    print(dp[n])
+    print(dp[t])
