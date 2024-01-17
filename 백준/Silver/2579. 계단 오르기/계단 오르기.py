fe24943b5745272f692d12d962ead395f1c407b2
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-01-17 17:18:26 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -7,10 +7,7 @@ if n == 1:
     print(step[0])
     exit(0)
 
-d = []
-d.append([0 , 0])
-d.append([step[0], 0])
-d.append([step[0] + step[1], step[1]])
+d = [[0 , 0], [step[0], 0], [step[0] + step[1], step[1]]]
 
 for i in range(3, n + 1):
     d.append([d[i - 1][1] + step[i - 1], max(d[i - 2]) + step[i - 1]])
