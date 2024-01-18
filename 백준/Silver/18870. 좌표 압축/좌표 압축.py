6c7817eed197178b5f1f2dc3884b3ac1281cdebf
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-01-18 15:04:22 +0900

    [Silver II] Title: 좌표 압축, Time: 1964 ms, Memory: 143264 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -7,19 +7,11 @@ lili = list(map(int, input().split()))
 li = list(set(lili))
 li.sort()
 
-def bs(find):
-    start = 0; end= len(li)
-    while start <= end:
-        mid = (start + end) // 2
-        if li[mid] == find:
-            return mid
-        elif li[mid] > find:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return 0
+d = dict()
+for l in range(len(li)):
+    d[li[l]] = l
 
 ret = []
 for l in lili:
-    ret.append(bs(l))
+    ret.append(d[l])
 print(*ret)
