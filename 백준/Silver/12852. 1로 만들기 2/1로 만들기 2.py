8448f7ccaadfb718358dbd4e38ab4b5d8a666cca
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-01-21 16:49:10 +0900

    [Silver I] Title: 1로 만들기 2, Time: 1168 ms, Memory: 150984 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -1,14 +1,13 @@
 n = int(input())
 
-d = [[0, 0], [0, 0]]
+d = [[0, 0] for _ in range(n + 1)]
 for i in range(2, n + 1):
-    tmp = []
-    if i % 3 == 0:
-        tmp.append([d[i // 3][0] + 1, i // 3])
-    if i % 2 == 0:
-        tmp.append([d[i // 2][0] + 1, i // 2])
-    tmp.append([d[i - 1][0] + 1, i - 1])
-    d.append(min(tmp, key=lambda x: x[0]))
+    d[i] = [d[i - 1][0] + 1, i - 1]
+    
+    if i % 3 == 0 and d[i // 3][0] + 1 < d[i][0]:
+        d[i] = [d[i // 3][0] + 1, i // 3]
+    if i % 2 == 0 and d[i // 2][0] + 1 < d[i][0]:
+        d[i] = [d[i // 2][0] + 1, i // 2]
 
 i = n
 ret = []
