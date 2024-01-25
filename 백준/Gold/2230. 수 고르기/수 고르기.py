352f0107cac6c2004788bd5138d2c08e0c6555d4
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-01-25 21:34:01 +0900

    [Gold V] Title: 수 고르기, Time: 160 ms, Memory: 35364 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,4 +1,21 @@
-from bisect import bisect_left, bisect_right
+# from bisect import bisect_left, bisect_right
+# import sys
+# input = sys.stdin.readline
+
+# n, m = map(int, input().split())
+# li = [int(input()) for _ in range(n)]
+# li.sort()
+
+
+# minVal = set()
+# for i in range(n - 1):
+#     target = m + li[i]
+#     left = bisect_left(li, target, i + 1)
+#     if left != len(li):
+#         minVal.add(li[left] - li[i])
+
+# print(min(minVal))
+
 import sys
 input = sys.stdin.readline
 
@@ -6,12 +23,15 @@ n, m = map(int, input().split())
 li = [int(input()) for _ in range(n)]
 li.sort()
 
-
-minVal = set()
-for i in range(n - 1):
-    target = m + li[i]
-    left = bisect_left(li, target, i + 1)
-    if left != len(li):
-        minVal.add(li[left] - li[i])
-
-print(min(minVal))
+start = 0; end = 1
+ret = set()
+while end < len(li):
+    if start == end:
+        end += 1
+        continue
+    if li[end] - li[start] >= m:
+        ret.add(li[end] - li[start])
+        start += 1
+        continue
+    end += 1
+print(min(ret))
