8be28c41564d785d1ba0410eadf9fd737abe8953
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-02-07 23:45:51 +0900

    [Silver II] Title: 최대 힙, Time: 120 ms, Memory: 38068 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,46 +1,15 @@
-import sys
+import sys,heapq
 input = sys.stdin.readline
 
-def heappush(li, x):
-  li.append(x)
-  now = len(li) - 1
-  while now > 0:
-    parent = (now - 1) // 2
-    if li[now] > li[parent]:
-      li[now], li[parent] = li[parent], li[now]
-      now = parent
-    else:
-      break
-
-def heappop(li):
-  li[0], li[-1] = li[-1], li[0]
-  del li[-1]
-  
-  now = 0
-  while now * 2 + 2 < len(li):
-    left = now * 2 + 1
-    right = now * 2 + 2
-    if li[left] >= li[right] and li[now] < li[left]:
-      li[now], li[left] = li[left], li[now]
-      now = left
-    elif li[left] <= li[right] and li[now] < li[right]:
-      li[now], li[right] = li[right], li[now]
-      now = right
-    else:
-      break
-  if now * 2 + 1 < len(li) and li[now] < li[now * 2 + 1 ]:
-    li[now], li[now * 2 + 1] = li[now * 2 + 1], li[now]
-
 n = int(input())
-hq = []; ret = []
-for i in range(n):
+hq = []; ret =[]
+for _ in range(n):
   x = int(input())
   if x != 0:
-    heappush(hq, x)
+    heapq.heappush(hq, x * -1)
   elif len(hq) == 0:
     ret.append(0)
   else:
-    ret.append(hq[0])
-    heappop(hq)
-if len(ret) != 0:
-  print(*ret, sep='\n')
+    ret.append(hq[0] * -1)
+    heapq.heappop(hq)
+print(*ret, sep='\n')
