3bdf51b2f3e048206806da57f9ae2313bdfa37b4
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-03-03 02:31:34 +0900

    [Silver III] Title: 두 수의 합, Time: 100 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -17,9 +17,22 @@ li = list(map(int, input().split()))
 li.sort()
 x = int(input())
 
-ret = 0
-for i in range(n - 1):
-  target = x - li[i]
-  if bisect(li, target, i + 1, n - 1):
-    ret += 1
-print(ret)
+# ret = 0
+# for i in range(n - 1):
+#   target = x - li[i]
+#   if bisect(li, target, i + 1, n - 1):
+#     ret += 1
+# print(ret)
+
+i = 0; j = n - 1
+cnt = 0
+while i < j:
+  tmp = li[i] + li[j]
+  if tmp == x:
+    cnt += 1
+    i += 1; j -= 1
+  elif tmp < x:
+    i += 1
+  else:
+    j -= 1
+print(cnt)
