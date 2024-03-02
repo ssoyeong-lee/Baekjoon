c3bbaaaad4df3d8c34298f2395b5fa17f703ff74
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-03-02 17:40:01 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3828 ms, Memory: 183512 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,3 +1,41 @@
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+
+# BIAS = 10 ** 6
+# delta = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+# n, m = map(int, input().split())
+# board = [input().rstrip() for _ in range(n)]
+# visited = [[-1] * m for i in range(n)]
+
+# def bfs():
+#   global visited
+#   visited[0][0] = 1
+
+#   queue = deque()
+#   queue.append((0,0))
+
+#   while queue:
+#     x, y = queue.popleft()
+    
+#     for dx, dy in delta:
+#       x1 = x + dx; y1 = y + dy
+#       if 0 <= x1 < m and 0 <= y1 < n:
+#         if visited[y1][x1] == -1:
+#           if board[y1][x1] == '0':
+#             visited[y1][x1] = visited[y][x] + 1
+#             queue.append((x1, y1))
+#           elif board[y1][x1] == '1' and visited[y][x] <= BIAS:
+#             visited[y1][x1] = visited[y][x] + 1 + BIAS
+#             queue.append((x1, y1))
+#         # elif board[y1][x1] == '0' and visited[y1][x1] % BIAS == visited[y][x] + 1:
+#         #   visited[y1][x1] = visited[y][x] + 1
+
+# bfs()
+# # print(*visited, sep='\n')
+# print(visited[-1][-1] % BIAS if not visited[-1][-1] == -1 else -1)
+
 import sys
 from collections import deque
 
@@ -20,9 +58,9 @@ def bfs():
     
     for dx, dy in delta:
       x1, y1 = x + dx, y + dy
-      if not (0 <= x1 < m and 0 <= y1 < n):
+      if not (0 <= x1 < m and 0 <= y1 < n and visited[y1][x1][cnt] == 0):
         continue
-      if board[y1][x1] == '0' and visited[y1][x1][cnt] == 0:
+      if board[y1][x1] == '0':
         visited[y1][x1][cnt] = visited[y][x][cnt] + 1
         queue.append((x1, y1, cnt))
       elif board[y1][x1] == '1' and cnt == 0:
