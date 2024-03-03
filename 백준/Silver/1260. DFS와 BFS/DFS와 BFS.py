a16510e008091d1f5b48c8253a06054005d9967d
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-03-03 18:13:03 +0900

    [Silver II] Title: DFS와 BFS, Time: 72 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -6,34 +6,69 @@ sys.setrecursionlimit(10 ** 6)
 delta = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 v, e, start = map(int, input().split())
 
-# 인접 행렬 방식으로 구현
-li = [[False] * (v + 1) for _ in range(v + 1)]
-for i in range(e):
+# # 인접 행렬 방식으로 구현
+# li = [[False] * (v + 1) for _ in range(v + 1)]
+# for i in range(e):
+#   v1, v2 = map(int, input().split())
+#   li[v1][v2] = li[v2][v1] = True
+
+# dfsRet = []
+# dfsVisited = [False] * (v + 1)
+# def dfs(now):
+#   dfsRet.append(now)
+#   dfsVisited[now] = True
+#   for i in range(1, v + 1):
+#     if li[now][i] and not dfsVisited[i]:
+#       dfsVisited[i] = True
+#       dfs(i)
+
+# bfsRet = []
+# bfsVisited = [False] * (v + 1)
+# def bfs(now):
+#   q = deque([now])
+#   bfsVisited[now] = True
+#   while q:
+#     node = q.popleft()
+#     bfsRet.append(node)
+#     for i in range(1, v + 1):
+#       if li[node][i] and not bfsVisited[i]:
+#         bfsVisited[i] = True
+#         q.append(i)
+
+# 인접 리스트 방식으로 구현
+li = [list() for _ in range(v + 1)]
+for _ in range(e):
   v1, v2 = map(int, input().split())
-  li[v1][v2] = li[v2][v1] = True
+  li[v1].append(v2)
+  li[v2].append(v1)
+
+for ll in li:
+  ll.sort()
 
 dfsRet = []
 dfsVisited = [False] * (v + 1)
-def dfs(now):
-  dfsRet.append(now)
-  dfsVisited[now] = True
-  for i in range(1, v + 1):
-    if li[now][i] and not dfsVisited[i]:
-      dfsVisited[i] = True
-      dfs(i)
+def dfs(n):
+  dfsVisited[n] = True
+  dfsRet.append(n)
+
+  for m in li[n]:
+    if not dfsVisited[m]:
+      dfsVisited[m] = True
+      dfs(m)
 
 bfsRet = []
 bfsVisited = [False] * (v + 1)
-def bfs(now):
-  q = deque([now])
-  bfsVisited[now] = True
+def bfs(n):
+  q = deque([n])
+  bfsVisited[n] = True
+
   while q:
-    node = q.popleft()
-    bfsRet.append(node)
-    for i in range(1, v + 1):
-      if li[node][i] and not bfsVisited[i]:
-        bfsVisited[i] = True
-        q.append(i)
+    now = q.popleft()
+    bfsRet.append(now)
+    for m in li[now]:
+      if not bfsVisited[m]:
+        bfsVisited[m] = True
+        q.append(m)
 
 dfs(start); bfs(start)
 print(*dfsRet)
