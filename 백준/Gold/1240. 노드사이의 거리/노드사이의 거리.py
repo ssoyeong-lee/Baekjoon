6d1b710918050ef13aceabf41d21700cd7b87223
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-03-07 17:15:26 +0900

    [Gold V] Title: 노드사이의 거리, Time: 112 ms, Memory: 38452 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -1,5 +1,5 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -8,22 +8,33 @@ for _ in range(n - 1):
   u, v, w = map(int, input().split())
   edges[u][v] = edges[v][u] = w
 
-def bfs(u, v):
-  dist = [0] * (n + 1)
-  queue = deque([u])
-  while queue:
-    cur = queue.popleft()
-    if cur == v:
-      break
-    for child in range(1, n + 1):
-      if dist[child] == 0 and edges[cur][child] > 0:
-        dist[child] = dist[cur] + edges[cur][child]
-        queue.append(child)
-  return dist[v]
+dist = [-1] * (n + 1)
+depth = [-1] * (n + 1)
+parent = [-1] * (n + 1)
+
+def dfs(root):
+  for child in range(1, n + 1):
+    if edges[root][child] > 0 and dist[child] == -1:
+      dist[child] = dist[root] + edges[root][child]
+      depth[child] = depth[root] + 1
+      parent[child] = root
+      dfs(child)
+
+dist[1] = depth[1] = parent[1] = 0
+dfs(1)
+
+def getGCP(u, v): # get Greatest Common Parent
+  while u != v:
+    if depth[u] > depth[v]:
+      u = parent[u]
+    else:
+      v = parent[v]
+  return u
 
 ret = [0] * m
 for i in range(m):
   u, v = map(int, input().split())
-  ret[i] = bfs(u, v)
+  gcp = getGCP(u, v)
+  ret[i] = dist[u] + dist[v] - 2 * dist[gcp]
 
 print(*ret, sep='\n')
