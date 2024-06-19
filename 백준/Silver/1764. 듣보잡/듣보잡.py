544e6d432f04e3b9b409c2f7da2fc9b45e58bf72
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-06-19 15:29:31 +0900

    [Silver IV] Title: 듣보잡, Time: 2720 ms, Memory: 44228 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+a = set([input() for _ in range(n)])
+b = set([input() for _ in range(m)])
+ret = list(a.intersection(b))
+ret.sort()
+print(len(ret))
+print(*ret, sep='\n')
