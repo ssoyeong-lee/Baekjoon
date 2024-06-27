eb4bc0125951dad6eb95cd0a03206cce4805e339
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-06-27 18:14:57 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1228 ms, Memory: 136500 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -9,21 +9,15 @@ table = [list(map(int, input().split())) for _ in range(n)]
 coord = [list(map(int, input().split())) for _ in range(m)]
 
 sumTable = [[-1] * (n + 1) for _ in range(n + 1)]
-for i in range(0, n + 1):
-  sumTable[0][i] = sumTable[i][0] = 0
-
-
-def calcSumTable(x, y):
-  if sumTable[y - 1][x] == -1:
-    calcSumTable(x, y - 1)
-  if sumTable[y][x - 1] == -1:
-    calcSumTable(x - 1, y)
-  sumTable[y][x] = table[y - 1][x - 1] + sumTable[y - 1][x] + sumTable[y][x - 1] - sumTable[y - 1][x - 1]
+for y in range(0, n + 1):
+  for x in range(0, n + 1):
+    if x == 0 or y == 0:
+      sumTable[y][x] = 0
+    else:
+      sumTable[y][x] = table[y - 1][x - 1] + sumTable[y - 1][x] + sumTable[y][x - 1] - sumTable[y - 1][x - 1]
 
 ret = []
 for y1, x1, y2, x2 in coord:
-  if sumTable[y2][x2] == -1:
-    calcSumTable(x2, y2)
   ret.append(sumTable[y2][x2] - sumTable[y2][x1 - 1] - sumTable[y1 - 1][x2] + sumTable[y1 - 1][x1 - 1])
 
 print(*ret, sep='\n')
