060e795b3b78e18179ddb273d1ecee4712da606f
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-06-28 01:03:17 +0900

    [Gold V] Title: LCS, Time: 564 ms, Memory: 55708 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -7,11 +7,10 @@ s2 = list(input().rstrip())
 
 dp = [[0] * (len(s1) + 1) for _ in range(len(s2) + 1)]
 
-for y in range(1, len(s2) + 1):
-  for x in range(1, len(s1) + 1):
-    if s1[x - 1] == s2[y - 1]:
-      dp[y][x] = dp[y - 1][x - 1] + 1
+for i, ss2 in enumerate(s2):
+  for j, ss1 in enumerate(s1):
+    if ss1 == ss2:
+      dp[i + 1][j + 1] = dp[i][j] + 1
     else:
-      dp[y][x] = max(dp[y - 1][x], dp[y][x - 1])
-
+      dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
 print(dp[-1][-1])
