9d36fe812089a2ec04205154d60045d504ec81eb
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-06-28 02:05:47 +0900

    [Gold V] Title: LCS, Time: 212 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -5,16 +5,13 @@ input = stdin.readline
 s1 = list(input().rstrip())
 s2 = list(input().rstrip())
 
-dp = [0] * (len(s1) + 1)
+dp = [0] * (len(s1))
 for i, ss2 in enumerate(s2):
-  prev = 0
+  lcs = 0
   for j, ss1 in enumerate(s1):
-    if ss1 == ss2:
-      tmp = dp[j + 1]
-      dp[j + 1] = prev + 1
-      prev = tmp
-    else:
-      prev = dp[j + 1]
-      dp[j + 1] = max(dp[j], dp[j + 1])
+    if lcs < dp[j]:
+      lcs = dp[j]
+    elif ss1 == ss2:
+      dp[j] = lcs + 1
 
-print(dp[-1])
+print(max(dp))
