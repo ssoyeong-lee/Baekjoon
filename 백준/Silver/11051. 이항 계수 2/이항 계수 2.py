56e4dc265831c958d9d9ddc999457bb3fa62cbc6
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-07-07 17:40:21 +0900

    [Silver II] Title: 이항 계수 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -1,12 +1,7 @@
 n, k = map(int, input().split())
+factorial = [1]
+for i in range(1, n + 1):
+  factorial.append(factorial[-1] * i)
 
-tri = []
-for i in range(n + 1):
-  tmp = []
-  for j in range(i + 1):
-    if i == 0 or j == 0 or i == j:
-      tmp.append(1)
-    else:
-      tmp.append((tri[-1][j - 1] + tri[-1][j]) % 10007)
-  tri.append(tmp)
-print(tri[n][k])
+# print(factorial)
+print((factorial[n] // factorial[n - k] // factorial[k]) % 10007)
