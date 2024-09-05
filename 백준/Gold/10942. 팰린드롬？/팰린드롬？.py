bd6da881261349791e045ee95625456f4c5cf2d4
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-09-05 21:59:46 +0900

    [Gold IV] Title: 팰린드롬?, Time: 1488 ms, Memory: 93208 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -1,28 +1,29 @@
 from sys import stdin
 input = stdin.readline
 
-def solution(numbers, query):
-  dp = [[False] * len(numbers) for _ in range(len(numbers))]
-  for length in range(len(numbers)):
-    for idx in range(length, len(numbers)):
-      if length == 0:
-        dp[length][idx] = True
-      elif length < 2 and numbers[idx - 1] == numbers[idx]:
-        dp[length][idx] = True
-      elif dp[length - 2][idx - 1] == True and numbers[idx - length] == numbers[idx]:
-        dp[length][idx] = True
+def solution(numbers):
+  dp = [[0] * len(numbers) for _ in range(len(numbers) + 1)]
+  for idx in range(len(numbers)):
+    dp[0][idx] = 1
+
+  for idx in range(1, len(numbers)):
+    if numbers[idx - 1] == numbers[idx]:
+      dp[1][idx] = 1
   
-  ret = [0] * len(query)
-  for i in range(len(query)):
-    s, e = query[i]
-    if dp[e - s][e - 1]:
-      ret[i] = 1
-  return ret
+  for length in range(2, len(numbers)):
+    for idx in range(length, len(numbers)):
+      if dp[length - 2][idx - 1] == 1 and numbers[idx - length] == numbers[idx]:
+        dp[length][idx] = 1
+  
+  return dp
 
 n = int(input())
 numbers = list(map(int, input().split()))
 m = int(input())
-query = [list(map(int, input().split())) for _ in range(m)]
 
-ret = solution(numbers, query)
+dp = solution(numbers)
+ret = [0] * m
+for i in range(m):
+  s, e = map(int, input().split())
+  ret[i] = dp[e - s][e - 1]
 print(*ret, sep='\n')
