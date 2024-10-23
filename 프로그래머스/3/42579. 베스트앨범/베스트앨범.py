8c8172cae3b6302adcdcdc56e971e80c2df7a2fb
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-10-23 18:13:35 +0900

    [level 3] Title: 베스트앨범, Time: 0.12 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -2,22 +2,21 @@ from collections import defaultdict
 
 def solution(genres, plays):
     total = defaultdict(int)
-    li = defaultdict(list)
+    songNum = defaultdict(list)
+
     for i in range(len(genres)):
         total[genres[i]] += plays[i]
-        li[genres[i]].append([i, plays[i]])
+        songNum[genres[i]].append([i, plays[i]])
     
-    tmp = []
+    total_list = []
     for t in total.keys():
-        tmp.append([t, total[t]])
-    total = sorted(tmp, key=lambda x:-x[1])
+        total_list.append([t, total[t]])
+    total = sorted(total_list, key=lambda x:-x[1])
     
     ret = []
     for genr, a in total:
-        tmp = li[genr]
-        tmp.sort(key=lambda x:(-x[1], x[0]))
-        
-        for i in range(min(2, len(tmp))):
-            ret.append(tmp[i][0])
+        song_per_genr = sorted(songNum[genr], key=lambda x:(-x[1], x[0]))
+        for num, cnt in song_per_genr[:2]:
+            ret.append(num)
     
     return ret
