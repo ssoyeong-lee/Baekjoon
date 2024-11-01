c44d24eb60761dda7f95fa0189414cc4d285d542
Lee Soyeong <77660581+ssoyeong-lee@users.noreply.github.com>
2024-11-02 00:27:13 +0900

    [level 2] Title: 타겟 넘버, Time: 607.58 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,19 +2,19 @@ cnt = 0
 target = 0
 numbers = []
 
-def backtracking(val, idx):
+def dfs(val, idx):
     global cnt
     if idx == len(numbers):
         if val == target:
             cnt += 1
         return
     
-    backtracking(val + numbers[idx], idx + 1)
-    backtracking(val + numbers[idx] * -1, idx + 1)
+    dfs(val + numbers[idx], idx + 1)
+    dfs(val + numbers[idx] * -1, idx + 1)
     
 def solution(ns, t):
     global target, numbers
     target = t
     numbers = ns
-    backtracking(0, 0)
+    dfs(0, 0)
     return cnt
